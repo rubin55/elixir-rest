e4f134219a7562ad43ad07b8cc5ebc7231f28628
Rubin Simons <me@rubin55.org>
2023-12-29 11:43:02 +0200

    Added benchee.

--- mix.exs
+++ mix.exs
@@ -23,7 +23,8 @@ defmodule ElixirRest.MixProject do
   defp deps do
     [
       {:plug_cowboy, "~> 2.5"},
-      {:jason, "~> 1.3"}
+      {:jason, "~> 1.3"},
+      {:benchee, "~> 1.0"}
     ]
   end
 end
